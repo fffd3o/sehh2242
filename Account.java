e8748f87f04b70eeedaef1d168c749d98662ca88
fffd3o <fffdog3@gmail.com>
2025-10-18 12:04:33 +0800

    Account.java

--- Account.java
+++ Account.java
@@ -59,6 +59,28 @@ public class Account
    } // end method getAccountNumber
 } // end class Account
 
+    class ChequeAccount extends Account {
+    private double chequeLimit; 
+    public ChequeAccount(int accountNumber, int pin, double availableBalance, 
+                         double totalBalance) {
+        super(accountNumber, pin, availableBalance, totalBalance);
+        chequeLimit = 50000;
+    }
+    public void debit(double amount){
+    super.totalBalance = totalBalance - amount;
+    System.out.println("get out " + amount + " new totalBalance is " + super.totalBalance);
+    }
+    
+    public void writeCheque(double amount) {
+        if (amount > chequeLimit) {
+            System.out.println("cannot");
+        } else if (amount > availableBalance) {
+            System.out.println("cannot");
+        } else {
+            debit(amount);
+        }
+    }
+}
 
 /**************************************************************************
  * (C) Copyright 1992-2007 by Deitel & Associates, Inc. and               *
